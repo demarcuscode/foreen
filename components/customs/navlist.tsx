06b040084b989c1b675364d3f3796dd86f66bb2e
Marcus <owaremarcuskofi@gmail.com>
2025-11-24 14:59:50 +0000

    third commit

--- components/customs/navlist.tsx
+++ components/customs/navlist.tsx
@@ -6,7 +6,7 @@ export default function NavList() {
   return (
     <div className="text-black">
       <SheetBtn />
-      <div className="md:flex items-center gap-4 hidden   ">
+      <div className="md:flex items-center gap-4 hidden ">
         <Link href="\dashboard">marcuoware@gmail.com</Link>
         <Link href="\library">my library</Link>
         <Link href="\profile">profile</Link>
